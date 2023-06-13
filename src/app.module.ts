a744e3391752d6cae0acc4f942c16170cbb359e5
nhandt <nhandt@nexlesoft.com>
2023-06-13 20:35:52 +0700

    add: env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { GoogleAuthModule } from "./google-auth/google-auth.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [GoogleAuthModule],
+  imports: [GoogleAuthModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
